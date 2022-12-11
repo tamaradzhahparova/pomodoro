9a62fd2ca05a87df4e83009a88535ffd798b82fd
Toma <ragimova-tamara@mail.ru>
2022-12-11 11:12:12 +0300

    connect to redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import 'normalize.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {store} from "./redux/store";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/redux/store.ts
@@ -0,0 +1,25 @@
+// import { createSlice, configureStore } from '@reduxjs/toolkit'
+//
+// const initialState: {
+//   todos: []
+// }
+//
+// const todoSlice = createSlice({
+//   name: 'todo',
+//   initialState,
+//   reducers: {
+//     addTodo: (state, action) => {
+//       state.todos = [...state.todos, action.payload]
+//     },
+//     decremented: state => {
+//       state.value -= 1
+//     }
+//   }
+// })
+//
+// export const { incremented, decremented } = counterSlice.actions
+//
+// export const store = configureStore({
+//   reducer: todoSlice.reducer
+// })
+//
