539cd8a06e7cc231e3f5516685d70d362fd84cd3
Toma <ragimova-tamara@mail.ru>
2022-12-19 20:35:31 +0300

    feat: add delete modal window

--- src/components/Todos/Dropdown/Dropdown.tsx
+++ src/components/Todos/Dropdown/Dropdown.tsx
@@ -4,33 +4,34 @@ import {Minus} from "../../icons/Minus";
 import {Edit} from "../../icons/Edit";
 import {Delete} from "../../icons/Delete";
 import {useAppDispatch} from "../../../redux/store";
-import {deleteTodo, increasePomodoroCount} from "../../../redux/slices/todos";
+import {increasePomodoroCount} from "../../../redux/slices/todos";
 import {FC} from "react";
 
 interface DropdownProps {
   taskId: number
+  setIsModalOpen: (value: boolean) => void
 }
 
-export const Dropdown: FC<DropdownProps> = ({taskId}) => {
+export const Dropdown: FC<DropdownProps> = ({taskId, setIsModalOpen}) => {
   const dispatch = useAppDispatch()
 
 
   return <ul className={s.dropdown}>
     <li className={s.dropdownItem} onClick={() => dispatch(increasePomodoroCount(taskId))}>
-        <Plus/>
-        <span>Увеличить</span>
+      <Plus/>
+      <span>Увеличить</span>
     </li>
     <li className={s.dropdownItem}>
-        <Minus/>
-        <span>Уменьшить</span>
+      <Minus/>
+      <span>Уменьшить</span>
     </li>
     <li className={s.dropdownItem}>
-        <Edit/>
-        <span>Редактировать</span>
+      <Edit/>
+      <span>Редактировать</span>
     </li>
-    <li className={s.dropdownItem} onClick={() => dispatch(deleteTodo(taskId))}>
-        <Delete/>
-        <span>Удалить</span>
+    <li className={s.dropdownItem} onClick={() => setIsModalOpen(true)}>
+      <Delete/>
+      <span>Удалить</span>
     </li>
   </ul>
 }
--- src/components/Todos/TodoItem/TodoItem.tsx
+++ src/components/Todos/TodoItem/TodoItem.tsx
@@ -1,7 +1,10 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import s from './TodoItem.module.css'
 import {DropdownIcon} from "../../icons/DropdownIcon";
 import {Dropdown} from "../Dropdown/Dropdown";
+import {Close} from "../../icons/Close";
+import {useAppDispatch} from "../../../redux/store";
+import {deleteTodo} from "../../../redux/slices/todos";
 
 interface TodoItemProps {
   name: string
@@ -10,18 +13,36 @@ interface TodoItemProps {
 }
 
 export const TodoItem: FC<TodoItemProps> = ({name, countOfPomodoro, id}) => {
-  return (<li className={s.todoItem}>
-    <div className={s.left}>
-      <div className={s.pomodoroCount}>{countOfPomodoro}</div>
-      <div className={s.name}>{name}</div>
-    </div>
-    <button className={s.dropdownButton}>
-      <DropdownIcon />
-      <div className={s.dropdownWrapper}>
-        <Dropdown taskId={id} />
-      </div>
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const dispatch = useAppDispatch()
 
-    </button>
-  </li>)
+  const deleteTodoCallback = () => {
+    dispatch(deleteTodo(id))
+    setIsModalOpen(false)
+  }
+
+  return (
+    <li className={s.todoItem}>
+      <div className={s.left}>
+        <div className={s.pomodoroCount}>{countOfPomodoro}</div>
+        <div className={s.name}>{name}</div>
+      </div>
+      <button className={s.dropdownButton}>
+        <DropdownIcon/>
+        {!isModalOpen && <div className={s.dropdownWrapper}>
+          <Dropdown taskId={id} setIsModalOpen={setIsModalOpen}/>
+        </div>}
+      </button>
+      {isModalOpen && <div className={s.modal}>
+        <div className={s.modalContent}>
+          <div className={s.modalText}>Удалить задачу?</div>
+          <button className={s.deleteButton} onClick={deleteTodoCallback}>Удалить</button>
+          <button className={s.cancelButton} onClick={() => setIsModalOpen(false)}>Отмена</button>
+          <button className={s.closeButton} onClick={() => setIsModalOpen(false)}>
+            <Close/>
+          </button>
+        </div>
+      </div>}
+    </li>)
 }
 
